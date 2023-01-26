216d7fcc8523aff2104db4ad8d11984e1ecad156
Ahmad Al Khatib <ahmadalkkhatib@gmail.com>
2023-01-26 20:53:32 +0200

    initial info provided

---
+++ Profile.java
@@ -0,0 +1,7 @@
+public class Profile{
+	public static void main(String[] args){
+		System.out.println("Ahmad Al Khatib");
+		System.out.println("Computer Engineering");
+		System.out.println("1st year");
+	}
+}
